56e401f276407787ad04d6d8f250674863e72d8c
Nguyen Duc Phu <72448688+ducphuE10@users.noreply.github.com>
2021-03-23 22:08:53 +0700

    Add files via upload

---
+++ Lab02/AimsProject/src/Aims.java
@@ -0,0 +1,10 @@
+
+public class Aims {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cart anOrder = new Cart();
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The lion King","Animation","Roger Allers",87,19.95f);
+	}
+
+}
